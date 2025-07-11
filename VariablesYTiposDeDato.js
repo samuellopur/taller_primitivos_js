1a6418e2f6513ff5fe4d863025d929540f5c1b67
Samuel Lopez <samuellopez01@hotmail.com>
2025-07-10 22:04:46 -0500

    Se realiza ajustes en el readme y se crea archivo VariablesYTiposDeDato

---
+++ VariablesYTiposDeDato.js
@@ -0,0 +1,13 @@
+//! Ejercicio 1
+
+let nombre = "Samuel";
+let edad = 38;
+let esEstudiante = true;
+
+//! Ejercicio 2
+
+let sinValor;
+let valorNulo = null;
+
+console.log("Variable sinValor => ", sinValor)
+console.log("Variable valorNulo => ", valorNulo)
